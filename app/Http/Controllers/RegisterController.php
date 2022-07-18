3c9e6ac78b1f14ce881ed129c015de2b7d039cf7
akash-ju16 <akash.ju.msc@gmail.com>
2022-07-18 10:24:16 +0600

    register update

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,19 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Register;
 
 class RegisterController extends Controller
 {
     //register data store
     public function store(Request $request)
     {
-        dd($request->input());
+        // dd($request->input());
+        $reg = new Register();
 
-        // $data = array(
-        //     $name => $request->input('full_name'),
-        //     $email => $request->input('email'),
-        //     $pass => $request->input('password'),
-        //     $confirm_password => $request->input('confirm_password'),
-        // );
+        $reg->full_name = $request->input('full_name');
+        $reg->email = $request->input('email');
+        $reg->password = $request->input('password');
+        $reg->confirm_password = $request->input('confirm_password');
+        $reg->user_level = 'editor';
+        $reg->status = 'inactive';
+
+        $reg->save();
     }
 }
