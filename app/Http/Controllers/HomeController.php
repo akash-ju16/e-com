6028fd9887b5d23e4f85c6ebdf364be12e7e259c
akash-ju16 <akash.ju.msc@gmail.com>
2022-10-12 19:05:09 +0600

    category menu display

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Categorie;
+use App\Models\{Categorie, Subcategorie, ChildCategory};
+use Illuminate\Support\Collection;
 
 class HomeController extends Controller
 {
@@ -13,8 +14,18 @@ class HomeController extends Controller
     */
     public function index()
     {
+        $collection = collect([1, 2, 3])->all();
+
+
+        $allCategory = Categorie::with(['Subcategorie', 'ChildCategory'])->get();
         echo '<pre>';
-        print_r(new Categorie);exit;
+        print_r($allCategory);
+        
+        foreach($allCategory as $item){
+            echo $item.'<br>';
+        }
+        exit;
+        
         return view('frontend.layouts.app');
     }
 }
