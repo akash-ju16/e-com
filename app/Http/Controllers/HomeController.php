84c65ea87e661f975640b1d902c1450387baef29
akash-ju16 <akash.ju.msc@gmail.com>
2022-10-12 18:44:43 +0600

    category menu display

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Categorie;
 
 class HomeController extends Controller
 {
     /** 
      * index page // home page
     */
-    public function index(){
+    public function index()
+    {
+        echo '<pre>';
+        print_r(new Categorie);exit;
         return view('frontend.layouts.app');
     }
 }
